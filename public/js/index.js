57ac2078a4a3fbc26ff09baa47a0937f440ecd21
mahadev <mahadevmirasdar@gmail.com>
2018-06-16 18:43:02 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,12 @@ var socket = io();
 
 socket.on('connect', function (){
   console.log('Connected to server');
-
-  socket.emit('createMessage',{
-    from:'tukaram@gmail.com',
-    text:'hey this is Tukaram'
-  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage',function (email) {
-  console.log('New Message',email);
+socket.on('newMessage',function(message) {
+  console.log('newMessage',message);
 });

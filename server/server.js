e4a2497b3d595fb4f23a7fa79a8b8305345c1f69
mahadev <mahadevmirasdar@gmail.com>
2018-06-26 16:26:27 +0530

    index

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation')
 
 
 const publicPath = path.join(__dirname, '../public');
@@ -25,6 +26,13 @@ io.on('connection',(socket) => {
 //socket.brodcast.emit from text New user joined
 
 socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined'));
+
+socket.on('join', (params, callback)=>{
+     if(!isRealString(params.name) || !isRealString(params.room)){
+         callback('Name and Room is Required.');
+     }
+     callback();
+});
 
 socket.on('createMessage', (message, callback) => {
      console.log('createMessage',message);
